7a3d0ebe448c6df2ac9110b37d6184a789f926da
shivambhagwani <125299494+shivambhagwani@users.noreply.github.com>
2023-03-15 15:58:37 -0400

    initial commits

--- src/Components/LandingPageCarousel.js
+++ src/Components/LandingPageCarousel.js
@@ -4,13 +4,13 @@ function LandingPageCarousel() {
   return (
     <Carousel>
       <Carousel.Item>
-      <div class="col-lg-3 text-center bg-secondary" style={{height:"600px", width:"auto"}}></div>
+      <div className="col-lg-3 text-center bg-secondary" style={{height:"600px", width:"auto"}}></div>
       </Carousel.Item>
       <Carousel.Item>
-      <div class="col-lg-3 text-center bg-primary" style={{height:"600px", width:"auto"}}></div>
+      <div className="col-lg-3 text-center bg-primary" style={{height:"600px", width:"auto"}}></div>
       </Carousel.Item>
       <Carousel.Item>
-      <div class="col-lg-3 text-center bg-danger" style={{height:"600px", width:"auto"}}></div>
+      <div className="col-lg-3 text-center bg-danger" style={{height:"600px", width:"auto"}}></div>
       </Carousel.Item>
     </Carousel>
   );
